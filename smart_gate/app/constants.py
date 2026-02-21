8c6f35fee9f90cb8fa1e49c97e040d5cfbb9c27f
andreaemmanuele <andreaemmanuele98@gmail.com>
2026-02-21 15:29:20 +0100

    test: fix hass url

--- smart_gate/app/constants.py
+++ smart_gate/app/constants.py
@@ -1,7 +1,7 @@
 import os
 
 SUPERVISOR_TOKEN = os.environ.get("SUPERVISOR_TOKEN")
-HASS_URL = "http://supervisor/core/api"
+HASS_URL = os.environ.get("HASS_URL", "http://supervisor/core/api")
 HEADERS = {
     "Authorization": f"Bearer {SUPERVISOR_TOKEN}",
     "Content-Type": "application/json"
